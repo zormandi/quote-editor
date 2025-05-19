5a3c4a6355932500b5c34e8d97f5f6208c8992bb
zormandi <zoltan.ormandi@gmail.com>
2025-05-19 17:27:37 +0200

    Broadcast quote changes with Turbo Streams

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,6 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(created_at: :desc) }
+
+  broadcasts_to ->(_quote) { "quotes" }, inserts_by: :prepend
 end
